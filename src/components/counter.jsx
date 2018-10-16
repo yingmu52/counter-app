5cbbd0ea302f512251e8331427bdefba5deb5ebd
Xinyi Zhuang <yingmu52@msn.com>
2018-10-16 12:50:31 -0700

    prefer stateless :)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Counter extends Component {
-
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className='btn btn-secondary btn-sm'> Increment
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2">Delete
-        </button>
-      </div>
-    );
-  }
-
-  formatCount = () => {
-    const { value } = this.props.counter;
-    return value === 0 ? 'Zero' : value;
-  }
-
-  getBadgeClasses = () => {
-    let classes = 'badge m-2 badge-';
-    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
-    return classes;
-  }
+const formatCount = (counter) => {
+  const { value } = counter;
+  return value === 0 ? 'Zero' : value;
 }
- 
+
+const getBadgeClasses = ({ value }) => {
+  let classes = 'badge m-2 badge-';
+  classes += value === 0 ? 'warning' : 'primary';
+  return classes;
+}
+
+const Counter = ({ onIncrement, onDelete, counter }) => {
+  return (
+    <div>
+      <span className={getBadgeClasses(counter)}>{formatCount(counter)}</span>
+      <button
+        onClick={() => onIncrement(counter)}
+        className='btn btn-secondary btn-sm'> Increment
+        </button>
+      <button
+        onClick={() => onDelete(counter.id)}
+        className="btn btn-danger btn-sm m-2">Delete
+        </button>
+    </div>
+  );
+}
+
+
+
 export default Counter;
